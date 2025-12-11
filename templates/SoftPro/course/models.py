366f8cca25ad02888bfc26deeef1cd4c30c527bb
UniversityAccount <bluecaspian1000@gmail.com>
2025-12-11 12:06:46 +0330

    Delete templates directory

--- templates/SoftPro/course/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Course(models.Model):
-    name = models.CharField(max_length=200)
-    code = models.CharField(max_length=30)
-    unit = models.IntegerField()
-    term = models.CharField(max_length=30)
-    prf_name = models.CharField(max_length=150)
-
